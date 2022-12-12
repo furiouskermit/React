341a3de25371dac0fd2389caae4a8f803bdc956e
furiouskermit <vndnsla12@gmail.com>
2022-12-12 22:29:46 +0900

    글씨 색상 변환 버튼

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,15 +1,46 @@
 import {useState} from "react";
 
+function Btn({text, color, onClick}) {
+  return (
+    <button
+      onClick={onClick}
+      data-color={color}
+      style={{
+        display: "inline-block",
+        width: 150,
+        height: 55,
+        marginRight: 10,
+        backgroundColor: color,
+        border: 0,
+        borderRadius: 7,
+        fontSize: 18,
+        color: "#fff"
+      }}
+    >{text}</button>
+  )
+}
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => (
     setCounter(counter+1)
   )
+
+  const [color, setColor] = useState("");
+  const colorChange = (event) => (
+    setColor((current) => (current = event.target.dataset.color))
+  )
   return (
     <div className="App">
-      <h1>Total Count: {counter}</h1>
-      <button onClick={onClick}>Click</button>
+      <h1 
+      style={{
+        color: color
+      }}>Total Count: {counter}</h1>
+      <button onClick={onClick}>Click me</button>
+      <div>
+        <Btn onClick={colorChange} color="tomato" text="1" />
+        <Btn onClick={colorChange} color="dodgerblue" text="2" />
+      </div>
     </div>
   );
 }
