13be1d887306856dab4d768c66fe5d71579e207e
furiouskermit <vndnsla12@gmail.com>
2023-01-10 20:45:38 +0900

    movie

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,48 +1,36 @@
-import {useState} from "react";
-
-function Btn({text, color, onClick}) {
-  return (
-    <button
-      onClick={onClick}
-      data-color={color}
-      style={{
-        display: "inline-block",
-        width: 150,
-        height: 55,
-        marginRight: 10,
-        backgroundColor: color,
-        border: 0,
-        borderRadius: 7,
-        fontSize: 18,
-        color: "#fff"
-      }}
-    >{text}</button>
-  )
-}
+import {useState, useEffect} from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const onClick = () => (
-    setCounter(counter+1)
-  )
-
-  const [color, setColor] = useState("");
-  const colorChange = (event) => (
-    setColor((current) => (current = event.target.dataset.color))
-  )
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=21e056381e595e8e0c4659d4441ba746").then(response => response.json()).then(json => {
+      setLoading(false);
+      setMovies(json.results)
+    })
+  }, [])
+  console.log(movies)
   return (
-    <div className="App">
-      <h1 
-      style={{
-        color: color
-      }}>Total Count: {counter}</h1>
-      <button onClick={onClick}>Click me</button>
-      <div>
-        <Btn onClick={colorChange} color="tomato" text="1" />
-        <Btn onClick={colorChange} color="dodgerblue" text="2" />
-      </div>
+    <div>
+      {loading ?
+        <h1>Loading...</h1> :
+        <ul className="movie-list">
+          {
+            movies.map((movie) => 
+            <li key={movie.id}>
+              <div className="movie-thumb"><img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} /></div>
+              <h2 className="movie-title">{movie.title}</h2>
+              <div className="movie-desc">
+                <span>Released: {movie.release_date}</span>
+                <span>★ {movie.vote_average}</span>
+              </div>
+            </li>)
+          }
+        </ul>
+      }
+      
     </div>
-  );
+  )
 }
 
 export default App;
